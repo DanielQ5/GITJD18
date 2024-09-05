3c7d8f32bde60763c0c95583923be0d71e8f3f6e
Daniel Quintanilla <daniel2356work@gmail.com>
2024-09-04 20:50:05 -0600

    Modificacion del Mensaje de Bienvenida en Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        String miTexto = "Bienvenidos a Java Developer";
+        System.out.println("Hola, " + miTexto);
+//        System.out.println("Hello and welcome!");
     }
 }
